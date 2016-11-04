c83cee9bfc43f96f7426ef46e8e60d9482b160d6
Kevin Albertson <ka324@scarletmail.rutgers.edu>
2016-11-03 21:59:04 -0400

    Add POC of using all valid utf-8 characters

--- web/print_strings.py
+++ web/print_strings.py
@@ -9,20 +9,33 @@ nullchar = 0 # ok for chrome, notepad++ and notepad, but breaks sublime auto det
 
 skipping = [newline, quot, carriageReturn, backslash, nullchar]
 
-file = open("strings.html", "w")
+file = open("strings2.html", "w")
+file.write("<html><head><meta charset='UTF-8'/></head><body>")
+file.write("<script>\nvar str=\"");
 
-file.write("<script>\nvar arr=[\n");
-for i in range(128):
-    if (i in skipping):
-        continue
-    file.write("\"a")
-    file.write(bytearray([i]))
-    file.write("\",\n")
-for i in range(0b11111 + 1):
-    for j in range(0b111111 + 1):
-        file.write("\"a")
-        file.write(bytearray([0b11000000 | i, 0b10000000 | j]));
-        file.write("\",\n")
-file.write("];\n</script>")
+invalidStart = 0b11000000
+invalidEnd = 0b11000010
+cap = 3000
+
+for i in range(0b11111111111 + 1):
+    b1 = 0b11000000 | ((i & 0b11111000000) >> 6);
+    b2 = 0b10000000 | (i & 0b00000111111);
+    if b1 >= invalidStart and b1 < invalidEnd: continue
+    print "Writing " + bin(b1) + " " + bin(b2);
+    file.write(bytearray([b1, b2]))
+    cap = cap - 1
+    if cap <= 0: break
+    
+
+# for i in range(0b11111 + 1):
+#     for j in range(0b111111 + 1):
+#         file.write(bytearray([0b11000010 | i, 0b10000000 | j]));
+
+# for i in range(128):
+#     if (i in skipping):
+#         continue
+#     file.write(bytearray([i]))
+
+file.write("\";\n</script></body></html>")
 file.close()
 print "done"
