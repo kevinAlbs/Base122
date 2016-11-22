8f5227fe3763f0e6630fe1c1d088ada6a1b394b5
Kevin Albertson <ka324@scarletmail.rutgers.edu>
2016-11-22 18:53:54 -0500

    Remove web directory

--- test/print_strings.py
+++ test/print_strings.py
@@ -1,21 +1,19 @@
-a = 65
-b = 66
-
+# Verify that all UTF-8 characters aside from the illegals here are parsed correctly by browsers.
 quot = 34
 newline = 10
 carriageReturn = 13
 backslash = 92
+ampersand = 38 # just breaks in HTML
 nullchar = 0 # ok for chrome, notepad++ and notepad, but breaks sublime auto detection...
 
-skipping = [newline, quot, carriageReturn, backslash, nullchar]
+skipping = [newline, quot, carriageReturn, backslash, ampersand, nullchar]
 
-file = open("strings2.html", "w")
+file = open("utf8-characters.html", "w")
 file.write("<html><head><meta charset='UTF-8'/></head><body>")
 file.write("<script>\nvar str=\"");
 
 invalidStart = 0b11000000
 invalidEnd = 0b11000010
-cap = 3000
 
 for i in range(128):
     if (i in skipping):
@@ -28,15 +26,6 @@ for i in range(0b11111111111 + 1):
     if b1 >= invalidStart and b1 < invalidEnd: continue
     print "Writing " + bin(b1) + " " + bin(b2);
     file.write(bytearray([b1, b2]))
-    cap = cap - 1
-    if cap <= 0: break
-    
-
-# for i in range(0b11111 + 1):
-#     for j in range(0b111111 + 1):
-#         file.write(bytearray([0b11000010 | i, 0b10000000 | j]));
-
-
 
 file.write("\";\n</script></body></html>")
 file.close()
